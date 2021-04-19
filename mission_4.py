9d8d4e1141c39324494182bb91c7e98fb454e32a
mebballa10 <med.balla.10@gmail.com>
2021-04-19 14:33:18 +0000

    ajouter la fonction position

--- mission_4.py
+++ mission_4.py
@@ -9,3 +9,24 @@ def is_adn(s):
     if k==len(s) and k!=0:
        tes=True
     return (tes)
+
+def liste(S):
+    """ fonction qui transforme une chaine à une liste de chaines de caractères
+        pre: prendre une chaine de caractère 'S'
+        post: avoir une liste de chaine de caractères'c'"""
+    c=[]
+    for i in range(len(S)):
+        c=c+[S[i:i+2]]
+        if i >=(len(S)-2):
+            break
+    return(c)
+def position(l,a):
+    """ fonction qui compare la variable 'a' avec les caractères de la liste de chaine de caractères 'l'
+        pre: prendre une liste de chaine de caractères 'l' et 'a'
+        post: avoir les positions où 'a'== 'l[i]' """
+    Q=liste(l)
+    P=[]
+    for i in range(len(Q)):
+        if a==Q[i] or a.upper()==Q[i] or a.lower()==Q[i]:
+                P=P+[i]   
+    return(P)
